21ae36d79cc05487b660435f60b50c08305e334b
Chris Trevino <chrisdadef@gmail.com>
2020-03-24 21:07:36 -0500

    yuhh

---
+++ 01week/datatypes.js
@@ -0,0 +1,11 @@
+'use strict'
+// write a javascript  \progrma to display ddate and time
+let now = new Date();
+console.log("the current date and time, now");
+
+//
+const theNumber = 5;
+const theString = theNumber.toString();
+
+console.log("string is", theNumber.tostring());
+console.log("the number is", theNumber);
