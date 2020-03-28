ef17d9b9d5f4943fd61800a5bb7a45bc52cea36d
Chris Trevino <chrisdadef@gmail.com>
2020-03-28 10:43:20 -0500

    internet down safe save

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,13 +1,20 @@
 'use strict'
-// 1.write a javascript  \progrma to display ddate and time
+// .1write a javascript  \progrma to display ddate and time
 let now = new Date();
 console.log("the current date and time, now");
 
-//.2
+//.2, 3
 const theNumber = 5;
 const theString = theNumber.toString();
 
 console.log("string is", theNumber.tostring());
 console.log("the number is", theNumber);
 
-//.3
+//.4
+const trueNumber = 50;
+const falseNumber = 40;
+
+  
+  console.log (typeof trueNumber <= falseNumber);
+
+//.5
