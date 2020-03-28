a0b7558fd5f086efad5663591cbfa74c1ff3ff95
Chris Trevino <chrisdadef@gmail.com>
2020-03-28 10:33:39 -0500

    updates

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,11 +1,13 @@
 'use strict'
-// write a javascript  \progrma to display ddate and time
+// 1.write a javascript  \progrma to display ddate and time
 let now = new Date();
 console.log("the current date and time, now");
 
-//
+//.2
 const theNumber = 5;
 const theString = theNumber.toString();
 
 console.log("string is", theNumber.tostring());
 console.log("the number is", theNumber);
+
+//.3
--- 01week/main.js
+++ 01week/main.js
@@ -1,7 +1,24 @@
 'use strict'
 
 console.log("inside my main.js file");
+//1.display the current time and date
+let myDate = new Date();
 
-let myDate = new Date(){
-  
-}
+console.log("The current date is", myDate);
+//2.gets the span from page/document
+    //let mySpan = document.getElementById("theTime");
+//3.change the inner text of the span says
+    //mySpan.innerText = myDate.toString();
+    //console.log()
+//.4
+  let mySpan = document.getElementById("theTime");
+   mySpan/addEventListener('click', function(){
+  mySpan.innerText = myDate.toString();
+  })
+
+
+
+//5.start point//
+
+
+
