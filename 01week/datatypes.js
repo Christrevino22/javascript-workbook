cb555cc190040cb7195545a6c95b801989483d5d
Chris Trevino <chrisdadef@gmail.com>
2020-03-28 11:22:27 -0500

    quicksave

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,20 +1,23 @@
 'use strict'
-// .1write a javascript  \progrma to display ddate and time
+// .1write a javascript program to display date and time
 let now = new Date();
 console.log("the current date and time, now");
 
-//.2, 3
+//.2, 3 converting a string to a number and so on
 const theNumber = 5;
 const theString = theNumber.toString();
 
-console.log("string is", theNumber.tostring());
+console.log("string is", theNumber.toString());
 console.log("the number is", theNumber);
 
 //.4
 const trueNumber = 50;
 const falseNumber = 40;
+const theSum = falseNumber + trueNumber;
 
   
+  console.log ( theSum );
   console.log (typeof trueNumber <= falseNumber);
 
 //.5
+
