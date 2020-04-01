b4f24cbbf1906beda721536edfd0f9f260983f65
Chris Trevino <chrisdadef@gmail.com>
2020-03-31 19:43:32 -0500

    rushed to finish

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -20,31 +20,76 @@ let trueNumber = 50;
 // let b = Number(prompt("Enter second number"));
 // alert(a + b);
  
-//.6 write a program than runs when two things are
-let x = 42;
-let y = 31;
- console.log(typeof x);
- console.log(typeof y);
+// //.6 write a program than runs when two things are
+// let x = 42;
+// let y = 31;
+//  console.log(typeof x);
+//  console.log(typeof y);
  
- let addBoth = (x, y) => {
-   if (x < y && y > 0) {
-     return (x - y)
-   }
- else {
-      return 'this will be False'
- }}
- addBoth(x, y);
+//  let addBoth = (x, y) => {
+//    if (x < y && y > 0) {
+//      return (x - y)
+//    }
+//  else {
+//       return 'this will be False'
+//  }}
+//  addBoth(x, y);
 
-//.7 
+//.7 Write a JavaScript program that runs when 1 of 2 things are true.
+
+const oneTrue = (input3, input4) => {
+
+  let a = document.getElementById("input3").value.trim().toLowerCase();
+  let b = document.getElementById("input4").value.trim().toLowerCase();
+
+  console.log(a);
+  console.log(b);
+
+
+  if (a == "null" || a == "nan" || a == "undefined" || a == "false" || a == "" || a == '' || a == "0" || a == "-0") {
+    a = null;
+  }
+  
+  if (b == "null" || b == "nan" || b == "undefined" || b == "false" || b == "" || b == '' || b == "0" || b == "-0") {
+    b = null;
+  }
+
+
+  console.log(a);
+  console.log(b);
+
+  console.log(typeof a);
+  console.log(typeof b);
 
 
 
 
 
 
+//8.
+const bothFalse = (input5, input6) => {
 
-//.5
+  let a = document.getElementById("input5").value.trim().toLowerCase();
+  let b = document.getElementById("input6").value.trim().toLowerCase();
+
+  console.log(a);
+  console.log(b);
 
 
-//.5
+  if (a == "null" || a == "nan" || a == "undefined" || a == "false" || a == "" || a == '' || a == "0" || a == "-0") {
+    a = null;
+  }
+  
+  if (b == "null" || b == "nan" || b == "undefined" || b == "false" || b == "" || b == '' || b == "0" || b == "-0") {
+    b = null;
+  }
+
+  console.log(a);
+  console.log(b);
+
+  console.log(typeof a);
+  console.log(typeof b);
+
+
+//.9
 
