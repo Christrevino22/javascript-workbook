90a586a9410dc5bde9a69ce8ba9d991e29d135c4
Chris Trevino <chrisdadef@gmail.com>
2020-04-06 12:43:14 -0500

    commit

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -16,24 +16,24 @@ let trueNumber = 50;
 
    console.log (typeof trueNumber <= falseNumber);
 // //.5 writing a program the add two things together. For some reason it works in repl.it but not here??
-// let a = Number(prompt("Enter first number"));
-// let b = Number(prompt("Enter second number"));
-// alert(a + b);
+let a = Number(prompt("Enter first number"));
+let b = Number(prompt("Enter second number"));
+alert(a + b);
  
 // //.6 write a program than runs when two things are
-// let x = 42;
-// let y = 31;
-//  console.log(typeof x);
-//  console.log(typeof y);
+let x = 42;
+let y = 31;
+ console.log(typeof x);
+ console.log(typeof y);
  
-//  let addBoth = (x, y) => {
-//    if (x < y && y > 0) {
-//      return (x - y)
-//    }
-//  else {
-//       return 'this will be False'
-//  }}
-//  addBoth(x, y);
+ let addBoth = (x, y) => {
+   if (x < y && y > 0) {
+     return (x - y)
+   }
+ else {
+      return 'this will be False'
+ }}
+ addBoth(x, y);
 
 //.7 Write a JavaScript program that runs when 1 of 2 things are true.
 
