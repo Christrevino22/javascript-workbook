fd6d2d6bc0c5f289b15d2a1263d73e4314d83d77
Chris Trevino <chrisdadef@gmail.com>
2020-06-14 22:48:44 -0500

    changes

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -13,6 +13,7 @@ let board = [
 ];
 
 let playerTurn = 'X';
+let Otherplayer = 'O';
 
 function printBoard() {
   console.log('   0  1  2');
@@ -22,25 +23,49 @@ function printBoard() {
   console.log('  ---------');
   console.log('2 ' + board[2].join(' | '));
 }
-
+/*
+*if a player won horizontally on any row, return true
+*Otherwise return false
+*/
 function horizontalWin() {
-  // Your code here
+  if(board[0][1] === playerTurn)
 }
-
+/*
+*if a player won vertically on any row, return true
+*Otherwise return false
+*
+*/
 function verticalWin() {
   // Your code here
 }
+/*
+*if a player won diagonally on any row, return true
+*Otherwise return false
+ */ 
 
 function diagonalWin() {
   // Your code here
 }
-
+// if they won H V D return true else return false
 function checkForWin() {
-  // Your code here
+  // code should call each check for types of wins
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  // check that row and column are valid(optional)
+  // check that row and column are empty(optional)
+
+  //update the board
+  board[row][column] = playerTurn;
+  //
+  //check if they won? if they did print nice message, (optional)
+  //maybe clear board(optional)
+ // set the player turn to be the other player(still need to figure out how to swap)
+
+
+
+ //code here should set marker on the board(CHECK)
+ // then check for win
 }
 
 function getPrompt() {
