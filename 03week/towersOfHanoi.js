2e60dd1f84380ec93ed285adba492d9cfc9e40c7
Chris Trevino <chrisdadef@gmail.com>
2020-06-23 21:15:16 -0500

    changes

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// assert libary is used for testing
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -18,29 +18,54 @@ function printStacks() {
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
-/*
+
+
+
+/** 
 This function takes in start Stack and end Stack,and moves the top piece
-*from the corresponding starting stack to the corresponding edning stack
-* @param {*}
-
+*from the corresponding starting stack to the corresponding ending stack
+* @param {*} startStack the name of the starting stack to move the piece from
+* @param {*} endStack the name of the ending stack where the moved piece should land
+*
 */
-
+  
 function movePiece(startStack, endStack) {
   // Your code here
   stacks.b.push('X')
 
 }
 
+/**
+ * This function takes in the startStack name  and endStack name
+ * and returns true only if the move is legal and valid
+ * otherwise it return false
+ * @param {*} startStack 
+ * @param {*} endStack 
+ */
+
 function isLegal(startStack, endStack) {
   // Your code here
+  
 
 }
-
+/**
+ * This function returns true, if the board is in a "winning state"
+ * winning state means : you restacked on either stacks b or c, biggest to smallest
+ * 
+ */
 function checkForWin() {         
   // Your code here
 
 }
 
+
+ /**
+  * This function should take in 2 inputs, the start stack and end stack and process that turn for the player
+  * @param {*} startStack the stack they want to move from 
+  * @param {*} endStack  the stack they want to move too
+  * check to see if move is legal first, if move is legal "move piece", then ckeck to see if it wins print WIN
+  * 
+  */
 function towersOfHanoi(startStack, endStack) {
   // Your code here
 
