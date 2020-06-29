af0e4810e09c80c94a1a690a402b21c0c7f7433b
Chris Trevino <chrisdadef@gmail.com>
2020-06-28 20:28:43 -0500

    chnages

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -9,6 +9,42 @@ let jobTypes = {
   programmer: 'Any Ship!'
 };
 
+class CrewMember {
+  constructor(name, job, specialSkill){
+  this.name = name;
+  this.job = job;
+  this.specialSkill = specialSkill;
+  this.ship = null;
+}
+
+ enterShip(ship){
+   this.ship = ship;
+   ship.crew.push(this);
+ }
+}
+
+class Ship { 
+  constructor(name, type, ability){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+    
+  }
+
+  missionStatement(){
+    if( this.crew.length == 0){
+      return "Can't perform a mission yet";
+    } else {
+      return this.ability;
+    }
+
+  }
+}
+let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
+ let crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+ let crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
 // Your code here
 
 //tests
