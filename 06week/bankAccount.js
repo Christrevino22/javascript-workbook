93922e888214fe7447716c0a0bccb5fda9ca9577
Chris Trevino <chrisdadef@gmail.com>
2020-07-08 18:47:51 -0500

    Not DOne

--- 06week/bankAccount.js
+++ 06week/bankAccount.js
@@ -2,31 +2,58 @@
 
 
 //create a Account Class
-class Account {
-  //create a contractor with with account #, owner, Transactions
+class BankAccount {
+  //the account number on this bankaccount
+  accountNumber;
+  //the name of the owner of the bank account
+  accOwner;
+  //array of Transaction instances against this bankaccount
+  transactions
+  //create a contractor with with account #, owner, Tran
   constructor(accountNum, accOwner, transactions) {
     this.accountNum = accountNum;
     this.accOwner = accOwner;
     this.transactions = [];
   }
-  //this method needs to return the sum of all the transactions
-  balance() {
-    let balanceOf = this.transactions.reduce(function(a, b){
-      return a + b;
-  });
-  
-  console.log(sum); // Hopefully this will print out the sum
-    
-  }
+  //this method , should take in an amount, and add a new Transaction
+  //as a deposit to this bank account  
+ deposit(amt){
 
-  deposit(amountDeposited) {
-    if (amountDeposited ) {
+    if(amt > 0) { //only allows deposits that are greater then zero
+
+      //1. create a transaction instance using amount passed in
+      let newTransaction = new Transaction(amt, "Deposit");
+      //2. add this newly created transaction to the transaction array
+      this.transactions.push(newTransaction);
 
     }
+ }
+ /**
+  * returns the current balance on the account by suming up this
+  * transactions amount
+  */
+balance(){
+
+    let sum = 0;
+
+    //how can you use higher order function here.
+    for(let i = 0; i < this.transactions.length; i++){
+      let currentTransaction = this.transactions[i];
+      sum = sum + currentTransaction.amount;
+    }
+    return sum;
   }
-  charge(payee, amountCharged){
-    if ()
+  charge(payee, amt){
+    /**
+     * TODO: currently this will allow me to overcharge
+     * add code to prevent overcharging.
+     * HINT: you can call the Balance() to get the balance before the charge
+     */
+
+     let newCharge = new Transaction(-amt, payee);
+     this.transactions.push(newCharge);
   }
+
 }
 class Transaction {
   constructor(amount ,payee) {
@@ -38,10 +65,11 @@ class Transaction {
 
 }
 
+/****************************** */
+let acct1 = new BankAccount("5553429", "John Doe");
 
-//created Account Owner
-let accountOne = new Account('454550', 'Christopher Trevino')
-//console.log(chrisAccount) 
-let transactionOne = new Transaction(50.50, 'Target')
-console.log(target);
+console.log(acct1.accountNum);
+console.log(acct1.accOwner);
+console.log(acct1.balance());
+
 
